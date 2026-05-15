dae51b36159fdec7c4af241d35abb098c04e0373
SrAlderson <srgonzalez1997@hotmail.com>
2026-05-14 19:11:10 -0500

    Initial commit - Project Structure

---
+++ src/main/java/org/empresaabc/salesmanagement/SgEmpresaAbcApplication.java
@@ -0,0 +1,13 @@
+package org.empresaabc.salesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgEmpresaAbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgEmpresaAbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/empresaabc/salesmanagement/SgEmpresaAbcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.empresaabc.salesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgEmpresaAbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
