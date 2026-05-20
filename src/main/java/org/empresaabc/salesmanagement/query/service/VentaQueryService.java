dc21907b1b9fb08136c5218b305404810f3b3be0
SrAlderson <srgonzalez1997@hotmail.com>
2026-05-20 12:45:36 -0500

    New Service Query for Ventas

---
+++ src/main/java/org/empresaabc/salesmanagement/query/service/VentaQueryService.java
@@ -0,0 +1,54 @@
+package org.empresaabc.salesmanagement.query.service;
+
+import lombok.RequiredArgsConstructor;
+import org.empresaabc.salesmanagement.query.document.VentaDocument;
+import org.empresaabc.salesmanagement.query.repository.VentaQueryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class VentaQueryService {
+
+    // ==========================================
+    // DEPENDENCIA REPOSITORY MONGO
+    // ==========================================
+
+    private final VentaQueryRepository
+            ventaQueryRepository;
+
+
+    // ==========================================
+    // CONSULTAR TODAS LAS VENTAS
+    // ==========================================
+
+    public List<VentaDocument>
+    obtenerVentas() {
+
+        return ventaQueryRepository
+                .findAll();
+    }
+
+
+    // ==========================================
+    // CONSULTAR POR CODIGO
+    // ==========================================
+
+    public VentaDocument
+    obtenerVentaPorCodigo(
+            String codigoVenta
+    ) {
+
+        return ventaQueryRepository
+                .findByCodigoVenta(
+                        codigoVenta
+                )
+                .orElseThrow(
+                        () ->
+                                new RuntimeException(
+                                        "Venta no encontrada"
+                                )
+                );
+    }
+}
