f1cb579a81f3ae78a971ab08fa56395377358884
SrAlderson <srgonzalez1997@hotmail.com>
2026-05-20 06:33:46 -0500

    New Events for Venta Creada

---
+++ src/main/java/org/empresaabc/salesmanagement/broker/event/DetalleVentaEvent.java
@@ -0,0 +1,20 @@
+package org.empresaabc.salesmanagement.broker.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetalleVentaEvent {
+
+    private String nombreArticulo;
+    private Integer cantidad;
+    private BigDecimal precioUnitario;
+    private BigDecimal subtotal;
+}
---
+++ src/main/java/org/empresaabc/salesmanagement/broker/event/VentaCreadaEvent.java
@@ -0,0 +1,49 @@
+package org.empresaabc.salesmanagement.broker.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.empresaabc.salesmanagement.shared.enums.EstadoVenta;
+import org.empresaabc.salesmanagement.shared.enums.TipoDocumento;
+import org.empresaabc.salesmanagement.shared.enums.TipoEntrega;
+import org.empresaabc.salesmanagement.shared.enums.TipoFactura;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VentaCreadaEvent {
+
+   //Datos relacionados con la venta
+
+    private String codigoVenta;
+    private LocalDateTime fechaVenta;
+    private String vendedor;
+    private EstadoVenta estadoVenta;
+    private TipoEntrega tipoEntrega;
+    private TipoFactura tipoFactura;
+    private BigDecimal totalVenta;
+
+    //Datos relacionados con el cliente
+
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private TipoDocumento tipoDocumento;
+    private String numeroDocumento;
+    private String correoCliente;
+    private String telefonoCliente;
+    private String direccionEntrega;
+    private String ciudad;
+
+    // Datos relacionados con la venta - DETALLE
+
+    private List<DetalleVentaEvent> detalles;
+}
