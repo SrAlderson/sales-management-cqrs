6768a357b3872c0a6606dacbf253e352a1c35d5f
SrAlderson <srgonzalez1997@hotmail.com>
2026-05-20 01:36:26 -0500

    New enum - Type of document

---
+++ src/main/java/org/empresaabc/salesmanagement/shared/enums/TipoDocumento.java
@@ -0,0 +1,9 @@
+package org.empresaabc.salesmanagement.shared.enums;
+
+//Valores permitidos para el tipo de documento
+public enum TipoDocumento {
+    CEDULA_CIUDADANIA,
+    TARJETA_IDENTIDAD,
+    CEDULA_EXTRANJERIA,
+    PASAPORTE
+}
